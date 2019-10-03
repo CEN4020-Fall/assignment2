3314140b3c3263ca72e0e8cfebdde8d8e3f7cb25
NoalGesler <55504625+NoalGesler@users.noreply.github.com>
2019-10-03 01:44:12 -0400

    Created Jet.cpp
    Jet.cpp defines functions of Jet.h, which inherits functionality from PoweredVehicle parent class.

---
+++ Jet.cpp
@@ -0,0 +1,56 @@
+#include "Jet.h"
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+Jet::Jet() {
+	myEngineSize = "unknown";
+	setBrand("unknown");
+	setModel("unkown");
+	setNumberOfEngines(1);
+}
+
+Jet::Jet(string brand, string model, string fuelType, string engineSize, int numberOfEngines) {
+	setBrand(brand);
+	setModel(model);
+	setFuelType(fuelType);
+	setEngineSize(engineSize);
+	setNumberOfEngines(numberOfEngines);
+}
+
+Jet::~Jet() = default;
+
+string Jet::getEngineSize() {
+	return myEngineSize;
+}
+
+void Jet::setEngineSize(string engineSize) {
+	if (engineSize == "unknown" || engineSize == "small" ||
+		engineSize == "medium" || engineSize == "grande") {
+		myEngineSize = engineSize;
+	}
+	else {
+		myEngineSize = "unknown";
+	}
+
+}
+
+double Jet::mileageEstimate(double time) {
+	srand((unsigned)time(0));
+	int mileage;
+	mileage = (rand() % 60) + 41;
+	if (fuelType == "Rocket" && numberOfEngines >= 2) {
+		double i = (numberOfEngines * 0.055) + 1;
+		mileage = mileage * i;
+	}
+	return mileage;
+}
+
+string Jet::toString() {
+	return "-> Jet\n" + PoweredVehicle::toString() + "\n\tEngine Size: " +
+		getEngineSize();
+}
+
+void Jet::setNumberOfEngines(int engineCount){
+	numberOfEngines = engineCount;
+}
