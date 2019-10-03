6f2c894aefa50cd54a7db4b1c29398fdf942f514
Taylor Rene Driver <driver@linprog1.cs.fsu.edu>
2019-10-03 17:46:34 -0400

    Created Skateboard.cpp File
    Edited repository by now adding the Skateboard.cpp file including all necessary functions to make the main file work correctly.

---
+++ Skateboard.cpp
@@ -0,0 +1,32 @@
+//
+//Created by Taylor Driver on 10/3/19
+//
+
+#include "Skateboard.h"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+Skateboard::Skateboard(string brand, string model) {
+	setbrand(brand);
+	setModel(model);
+}
+
+Skateboard::~Skateboard() = default;
+
+double Skateboard::mileageEstimate(double time) {
+	srand((unsigned)time(0));
+
+	double mileage = (rand() % .5) + .1;
+
+	if(time>25 && time < 250)
+		mileage = mileage + ((rand() % time/3) + 1);  
+
+}
+
+string Skateboard::toString() {
+	string s = "-> Skateboard\n\t";
+	return "-> Skateboard\n" + Vehicle::toString(); 
+
+
+}
