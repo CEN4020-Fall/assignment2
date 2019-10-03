1be310110c5192e53995fd25d6dfa10565e47299
cflynn0 <55844079+cflynn0@users.noreply.github.com>
2019-10-02 22:50:08 -0400

    Set up Jet header and Jet cpp

--- Jet.cpp
+++ Jet.cpp
@@ -1,1 +1,22 @@
 //Jet.cpp
+
+#include "Jet.h"
+
+Jet::Jet() {
+    setFuelType("unknown");
+}
+
+Jet::~Jet() = default;
+
+string Jet::getFuelType() {
+    return fuelType;
+}
+
+void Jet::setFuelType(string fuel) {
+    fuelType = fuel;
+}
+
+string Jet::toString() {
+    return Vehicle::toString() + "\n\tFuelType: " + getFuelType();
+}
+
--- Jet.h
+++ Jet.h
@@ -1,1 +1,27 @@
-//Jet.h
+//
+// Conner Flynn (cjf16e) 10/2/19
+//
+
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "PoweredVehicle.h"
+
+class Jet : public PoweredVehicle {
+
+private:
+	int numberOfEngines;
+	
+public:
+	Jet();
+	
+	explicit Jet(string brand, string model, string fuelType, int numberOfEngines = 1);
+	
+	virtual ~Jet();
+	string getFuelType();
+	virtual string toString();
+	virtual double mileageEstimate(double time);
+};
+
+
+#endif //DRIVINGSIMULATOR_JET_H
