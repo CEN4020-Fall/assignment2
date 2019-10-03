ee7ccd8adc658bfb2999ad60ff1aa3775fb281d9
NoalGesler <55504625+NoalGesler@users.noreply.github.com>
2019-10-03 01:43:24 -0400

    Created Jet.h
    New class type Jet.h, child class which inherits functionality from PoweredVehicle class

---
+++ Jet.h
@@ -0,0 +1,27 @@
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "PoweredVehicle.h"
+
+class Jet : public PoweredVehicle {
+
+private:
+	int numberOfEngines = 1;
+	string myEngineSize;
+
+public:
+	Jet();
+
+	explicit Jet(string brand, string model, string fuelType,
+		string engineSize, int numberOfEngines);
+
+	virtual ~Jet();
+	string getEngineSize();
+	void setEngineSize(string engineSize);
+	virtual double mileageEstimate(double time);
+	virtual string toString();
+	int setNumberOfEngines(int engineCount)
+};
+
+
+#endif //DRIVINGSIMULATOR_JET_H
