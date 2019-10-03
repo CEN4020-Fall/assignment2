ffb4e000d4e749ff253f5dca14b69995120608b5
Taylor Rene Driver <driver@linprog6.cs.fsu.edu>
2019-10-03 19:26:16 -0400

    Added Test Cases
    Edited repository by adding test cases for Jet, Skateboard, and RollerCoaster in the main.cpp file.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ void printVehiclesRoster(Vehicle **vehicles, int size);
 int main() {
     std::cout << "Driving simulator" << std::endl;
     int size = 6;
-    int capacity = 10;
+    int capacity = 12;
     Vehicle **vehiclesArray = new Vehicle *[capacity];
 
     vehiclesArray[0] = new Car();
@@ -16,6 +16,12 @@ int main() {
     vehiclesArray[3] = new Car("Tesla", "T2", "electricity", "large");
     vehiclesArray[4] = new Bicycle("Mizuno", "Wave", 10);
     vehiclesArray[5] = new Car("BMW", "X5", "diesel", "grande");
+    vehiclesArray[6] = new Jet("Cessna", "Citation", "diesel", "grande");
+    vehiclesArray[7] = new Jet("Learjet", "31", "diesel", "grande");
+    vehiclesArray[8] = new Skateboard("Zero", "Sandoval");
+    vehiclesArray[9] = new Skateboard("Element", "Jaakko");
+    vehiclesArray[10] = new RollerCoaster("Intamin","Taron", "Steel","diesel", "medium"); 
+    vehiclesArray[11] = new RollerCoaster("Vekoma", "Invertigo", "Steel", "diesel", "grande");
 
     printVehiclesRoster(vehiclesArray, size);
 
