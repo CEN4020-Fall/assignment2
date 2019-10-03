80b00a3a44b31b0209de3b9d17b60918edacf674
Taylor Rene Driver <driver@linprog2.cs.fsu.edu>
2019-10-03 18:07:13 -0400

    Added RollerCoaster.h File
    Edited repository by adding the RollerCoaster.h file to create my final PoweredVehicle.

---
+++ RollerCoaster.h
@@ -0,0 +1,27 @@
+//
+// Created by Taylor Driver on 10/3/19
+//
+
+#ifndef DRIVIGSIMULATOR_ROLLERCOASTER_H
+#define DRIVINGSIMULATOR_ROLLERCOASTER_H
+
+#include "PoweredVehicle.h"
+
+class RollerCoaster : public PoweredVehicle {
+
+private:
+	string myEngineSize;
+
+public:
+	RollerCoaster();
+
+	explicit RollerCoaster(string manufacturer, string model, string fuelType, string engineSize);
+
+	virtual ~Rollercoaster();
+	string getEngineSize();
+	void setEngineSize(String engineSize);
+	virtual double mileageEstimate(double time);
+	virtual string toString();
+};
+
+#endif //DRIVINGSIMULATOR_ROLLERCOASTER_H
