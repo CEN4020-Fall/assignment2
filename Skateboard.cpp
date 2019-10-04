855f74e4f8c55e4f62c9a0cb778f0b05dc04919b
Taylor Rene Driver <driver@linprog8.cs.fsu.edu>
2019-10-04 18:29:11 -0400

    Final Commit
    All bugs and typos have been fixed in the Skateboard.cpp file.

--- Skateboard.cpp
+++ Skateboard.cpp
@@ -6,6 +6,8 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <cmath>
+#include <random>
 
 Skateboard::Skateboard(string brand, string model) {
 	setBrand(brand);
@@ -15,13 +17,23 @@ Skateboard::Skateboard(string brand, string model) {
 Skateboard::~Skateboard() = default;
 
 double Skateboard::mileageEstimate(double time) {
-	srand((unsigned)time(0));
+	//double mileage = .5 + (std::rand() % (.5-.1+1)) //(double) rand()/(.5 + 1) + .1 + ((double)rand()%.4);
+	//mileage = (int) floor(mileage);
+	//double mileage = rand() * 1.0 / .5 * (.5-.1+1) + .1;
+	//used Cubbi's answer on StackOverflow's "random number c++ in some range"
 
-	double mileage = (rand() % .5) + .1;
+	std::random_device rd;
+	std::mt19937 rng(rd());
+	std::uniform_real_distribution<double> dist(0.1,0.5);
+	double randnum = dist(rng);
 
-	if(time>25 && time < 250)
-		mileage = mileage + ((rand() % time/3) + 1);  
+	double mileage = randnum * time;
 
+	if(time>25.0 && time < 250.0){
+		//mileage = mileage + (double)rand()/(time/3) + 1;  
+		std::uniform_real_distribution<double> dist(0.1,time/3);
+		mileage = mileage + dist(rng);
+	}
 }
 
 string Skateboard::toString() {
