462145ee2ff7f5f915f32fafe4f8e6fdc3cc3dcd
Taylor Rene Driver <driver@linprog8.cs.fsu.edu>
2019-10-04 18:28:21 -0400

    Final Commit
    All bugs and typos have been fixed in the RollerCoaster.cpp file.

--- RollerCoaster.cpp
+++ RollerCoaster.cpp
@@ -6,8 +6,9 @@
 
 RollerCoaster::RollerCoaster() {
 	myEngineSize = "unknown";
-	setManufacturer("B&M");
-	setModel("Wooden");
+	myManufacturer = "unknown";
+	myName = "unknown";
+	myModel = "unknown";
 }
 
 RollerCoaster::RollerCoaster(string manufacturer, string name, string model, string fuelType, string engineSize) {
@@ -25,18 +26,42 @@ string RollerCoaster::getEngineSize() {
 	return myEngineSize;
 }
 
+string RollerCoaster::getManufacturer() {
+	return myManufacturer;
+}
+
+string RollerCoaster::getName() {
+	return myName;
+}
+
+string RollerCoaster::getModel() {
+	return myModel;
+}
+
 void RollerCoaster::setEngineSize(string engineSize) {
-	if(engineSize == "unknown" || engineSize == "small" || engineSize == "medium" || engineSize == "grande") {
+	if(engineSize == "unknown" || engineSize == "small" || engineSize == "medium" || engineSize == "grande") 
  		myEngineSize = engineSize;
 	else
 		myEngineSize = "unknown";
 }
 
-double RollerCoaster::mileageEstimate(double time) {
-	srand((unsigned)time(0));
-	//double mileage = time * 20;
+void RollerCoaster::setManufacturer(string manufacturer) {
+	myManufacturer = manufacturer;
+}
 
-	if(model == "Wooden") {
+void RollerCoaster::setName(string name) {
+	myName = name;
+}
+
+void RollerCoaster::setModel(string model) {
+	myModel = model;
+}
+
+double RollerCoaster::mileageEstimate(double time) {
+	//srand((unsigned)time(0));
+	double mileage = time * 20;
+
+	if(myModel == "Wooden") 
 		double mileage = time * ((rand() % 72-4) + 5);
 	
 		
@@ -45,5 +70,5 @@ return mileage;
 
 
 string RollerCoaster::toString() {
-	return "-> RollerCoaster\n" + PoweredVehicle::toString() + "\n\tEngine Size: " + getEngineSize();
+	return "-> RollerCoaster" + std::string("\n\tManufacturer: ") + getManufacturer() + std::string("\n\tModel: ") + getModel() + std::string("\n\tName: ") + getName() + std::string("\n\tFuel Type: ") + getFuelType() + std::string("\n\tEngine Size: ") + getEngineSize();
 }
