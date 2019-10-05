59e016e004e80189c06bc57cf0b0afb2a19ed2db
NickWattsCS <nsw12b@my.fsu.edu>
2019-10-04 20:49:26 -0400

    Added interface to Jet.h
    All function prototypes added in Jet.h are modeled after those in Bicycle.h and Car.h

---
+++ Jet.h
@@ -0,0 +1,26 @@
+//This file was written by Nicholas Watts on Oct 4, 2019.
+//Code modeled on Bicycle.h and Car.h in parts, with differences
+//in implementation annotated.
+
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "PoweredVehicle.h"
+
+class Jet: public PoweredVehicle {
+
+private:
+	int numberOfEngines;
+
+public:
+	Jet();
+	explicit Jet(string brand, string model, string fuelType,
+			int engineNum);		//number of engines to be copied to private data
+	virtual ~Jet();
+	int getNumberEngines();			//NW added
+	void setNumberEngines(int newNum);	//NW added
+	virtual double mileageEstimate(double time);
+	virtual string toString();
+};
+
+#endif
