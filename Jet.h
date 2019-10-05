92e17949bed15ad133dd41c0b43c1820639df502
aap17e <44821710+aap17e@users.noreply.github.com>
2019-10-04 21:03:15 -0400

    Create Jet.h
    o A jet has an additional member data “numberOfEngines” (int). It is an integer with
    default value of 1.
    o Mileage = A random number between 40 and 100 miles per minute. If the Jet has
    more than 2 engines and the fuel type is “Rocket”, the mileage will be boosted by
    5.5% per engine. Jet inherits from the PoweredVehicle class.

---
+++ Jet.h
@@ -0,0 +1,25 @@
+
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "PoweredVehicle.h"
+#include "Vehicle.h"
+
+class Jet : public PoweredVehicle {
+
+private:
+    int numberOfEngines;
+
+public:
+    explicit Jet(string brand, string model, int numberOfEngines = 1);
+
+    virtual ~Jet();
+    int getEngineCount();
+    void setEngineCount(int gearCount);
+    virtual double mileageEstimate(double time);
+
+    virtual string toString();
+};
+
+
+#endif
