66c70cb7779c9b5374cbcb1838ffa1cc293f9e43
aap17e <44821710+aap17e@users.noreply.github.com>
2019-10-04 22:31:00 -0400

    Update Jet.h

--- Jet.h
+++ Jet.h
@@ -1,4 +1,6 @@
-
+//
+// Created by Andres Paz
+//
 #ifndef DRIVINGSIMULATOR_JET_H
 #define DRIVINGSIMULATOR_JET_H
 
@@ -16,6 +18,7 @@ public:
     virtual ~Jet();
     int getEngineCount();
     void setEngineCount(int gearCount);
+    virtual ~Jet();
     virtual double mileageEstimate(double time);
 
     virtual string toString();
