1b0d594416b5ffe7b238ac340362b0adc0f218c0
aap17e <44821710+aap17e@users.noreply.github.com>
2019-10-04 21:05:35 -0400

    Created Skateboard.h
    Mileage = A random number between 0.1 and 0.5 miles per minute. If time is more
    than 25 minutes and less than 250 minutes, add a random amount of mileage, ranging
    from to 1 to a third of the time’s value. Skateboard inherits from the Vehicle class

---
+++ Skateboard.h
@@ -0,0 +1,20 @@
+#ifndef DRIVINGSIMULATOR_SKATEBOARD_H
+#define DRIVINGSIMULATOR_SKATEBOARD_H
+
+#include "Vehicle.h"
+
+class Skateboard : public Vehicle {
+
+private:
+
+public:
+    explicit Skateboard(string brand, string model);
+
+    virtual ~Skateboard();
+    virtual double mileageEstimate(double time);
+
+    virtual string toString();
+};
+
+
+#endif
