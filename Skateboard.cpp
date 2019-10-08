d45cf9741515b81921bf2fd24969f9292e50a853
Robby Jones <rfj15@my.fsu.edu>
2019-10-07 23:12:14 -0400

    citation

--- Skateboard.cpp
+++ Skateboard.cpp
@@ -18,7 +18,10 @@ Skateboard::~Skateboard() = default;
 double Skateboard::mileageEstimate(double myTime) {
     srand( (unsigned int)time( NULL ));
     double max = 0.5, min = 0.1;
+
+    //https://stackoverflow.com/questions/10776073/random-double-between-min-and-max
     double mileage = (rand()*(max-min)/((double)RAND_MAX-min))+min;
+
     if (myTime > 25 && myTime < 250){
         min = 1;
         max = myTime*1/3;
