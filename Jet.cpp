a82a51f03c24ae4c46e8bc226e869dd6bde7cfbf
Robby Jones <rfj15@my.fsu.edu>
2019-10-07 22:59:21 -0400

    Update Jet class

--- Jet.cpp
+++ Jet.cpp
@@ -11,6 +11,8 @@ Jet::Jet(string fuelType, int numEngines){
     setNumEngines(numEngines);
 }
 
+Jet::~Jet() = default;
+
 int Jet::getNumEngines() {
     return numberOfEngines;
 }
@@ -20,16 +22,9 @@ void Jet::setNumEngines(int numEngines) {
 }
 
 double Jet::mileageEstimate(double time) {
-    /*srand((unsigned int) time(NULL));
-    mileage = rand();
-    double mileage = time;
-    double max = 0.5, min = 0.1, range;
-    range = max - min + 0.1;
-    double fin = rand()*1.0;
-    mileage = fmod(fin, range+min);
-    cout << mileage;
+    double mileage = rand()%60 + 40;
     if(numberOfEngines > 2 && fuelType == "Rocket"){
-        mileage += mileage*0.055;
-    }*/
-    return time;
+        mileage += 0.055*numberOfEngines;
+    }
+    return mileage;
 }
